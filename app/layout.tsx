d0caf072dc18a038adb9c5942db2f78be4508b2e
theisolatedmonk <96rahulseth@gmail.com>
2023-11-02 23:31:27 +0530

    font imported

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;500;700&display=swap" rel="stylesheet"></link>
       <body className={inter.className}>{children}</body>
     </html>
   )
